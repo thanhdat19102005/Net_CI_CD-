6250d063be85f44fecd0df8b6f9c6aa457c56616
thanhdat19102005 <hope09023@gmail.com>
2026-08-13 11:45:10 +0700

    add file new controller

---
+++ ClothHub/Controllers/TestCICDController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCICDController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult CheckStatus()
+        {
+            return Ok(new
+            {
+                Status = "Success",
+                Message = "Tuyệt vời! Jenkins CI/CD Pipeline đang hoạt động hoàn hảo!",
+                Version = "V1 - Cập nhật lúc " + DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy"),
+                Environment = "Production - MonsterASP"
+            });
+        }
+    }
+}
--- ClothHub/obj/Debug/net8.0/ClothHub.AssemblyInfo.cs
+++ ClothHub/obj/Debug/net8.0/ClothHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8cd89f0295109884cf09a8ad871bddfca5eb5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228053a7357a85ca843df72506223e7f5f6a598f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
